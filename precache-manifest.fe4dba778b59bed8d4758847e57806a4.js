65e884b364ca6d30b1eed127a7edda2f1a3356b5
RAj Kumar Chaudhary <chaudharyrajkumar173@gmail.com>
2021-09-03 15:55:23 +0530

    Updates

---
+++ precache-manifest.fe4dba778b59bed8d4758847e57806a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76a0f61957caf6ac816b",
+    "url": "/TrelloApp/static/css/main.e3b91d1a.chunk.css"
+  },
+  {
+    "revision": "76a0f61957caf6ac816b",
+    "url": "/TrelloApp/static/js/main.76a0f619.chunk.js"
+  },
+  {
+    "revision": "2b97a9bb720685762ddb",
+    "url": "/TrelloApp/static/js/runtime~main.2b97a9bb.js"
+  },
+  {
+    "revision": "0f073564a51a3b72c639",
+    "url": "/TrelloApp/static/js/2.0f073564.chunk.js"
+  },
+  {
+    "revision": "92649e372ecab4f3c1dfc803d2fe5624",
+    "url": "/TrelloApp/index.html"
+  }
+];
